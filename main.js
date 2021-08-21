99792a430db4a98b204f74af27226b60090768e8
Subodhthore15 <47239753+Subodhthore15@users.noreply.github.com>
2021-08-21 16:17:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+function heart_red() {
+        // let heart = document.querySelector('.heart');
+        let heart = document.getElementsByClassName('heart')[0];
+
+        let likes = document.getElementsByClassName('likes')[0];
+
+            if (heart.src.match('/img/heart.png')) 
+            {
+                heart.src = '/img/heart_red.png';
+                likes.innerHTML="3,445 likes"
+            }
+            else{
+                heart.src = '/img/heart.png';
+                likes.innerHTML="3,444 likes"
+            }
+
+        }
+
